e874ffbc59b3de1322e86d8f16a84ffbf1bcb1a9
Prachi3899 <116147549+Prachi3899@users.noreply.github.com>
2024-12-25 23:19:14 +0530

    about section created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import About from "./components/sections/about";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  width: 100%;
+  height: 90%;
+  overflow-x: hidden;
+  position: relative;
+`;
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
       <Navbar />
+      <Body>
+        <About />
+      </Body>
       </BrowserRouter>
     </ThemeProvider>
   );
